05b126b9d3866f19b1858a6eb00d32633b12f9d7
Jacques Lucke <mail@jlucke.com>
2019-02-24 22:33:38 +0100

    cleanup

--- main.cpp
+++ main.cpp
@@ -4,35 +4,10 @@
 #include <iostream>
 #include <stdint.h>
 
-uint32_t my_hash(const int &v) NOINLINE {
-    uint32_t value = v;
-    value = ~value + (value << 15);
-    value = value ^ (value >> 12);
-    value = value + (value << 2);
-    value = value ^ (value >> 4);
-    value = value * 2057;
-    value = value ^ (value >> 16);
-    return value;
-}
+using IntSet = HashSet<int, HashBits32>;
+using StringSet = HashSet<std::string, HashString>;
 
 int main() {
-    HashSet<int, HashBits32> set;
-
-    set.insert_new(5);
-    set.insert_new(7);
-    set.insert_new(2);
-    set.insert_new(3);
-
-    for (int i = 100; i < 10000; i++) {
-        if (i % 4 == 0) {
-            set.insert_new(i);
-        }
-    }
-
-    set.remove(500);
-
-    for (uint i = 490; i < 510; i++) {
-        bool contained = set.contains(i);
-        std::cout << i << ": " << contained << std::endl;
-    }
+    StringSet set;
+    set.insert("Hello World");
 }
