5f0b8175570df5e0a3d736ea05a4f6dd341a9f1c
Jacques Lucke <mail@jlucke.com>
2019-02-26 11:11:41 +0100

    benchmark unordered set

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -1,11 +1,27 @@
 #include "hash_set.hpp"
 #include "hashing.hpp"
 #include <benchmark/benchmark.h>
+#include <unordered_set>
 
 using IntSet = HashSet<int, HashBits32>;
 
 static void BM_HashSet_Insert(benchmark::State &state) {
     IntSet set;
+    for (auto _ : state) {
+        state.PauseTiming();
+        set = {};
+        state.ResumeTiming();
+        for (int i = 0; i < state.range(0); i++) {
+            set.insert(i);
+        }
+    }
+    state.SetItemsProcessed(state.iterations() *
+                            state.range(0));
+}
+
+static void
+BM_UnorderedSet_Insert(benchmark::State &state) {
+    std::unordered_set<int> set;
     for (auto _ : state) {
         state.PauseTiming();
         set = {};
@@ -50,6 +66,7 @@ BM_HashSet_BuildFromVector(benchmark::State &state) {
 }
 
 BENCHMARK(BM_HashSet_Insert)->Range(8, 8 << 20);
+BENCHMARK(BM_UnorderedSet_Insert)->Range(8, 8 << 20);
 BENCHMARK(BM_HashSet_InsertNew)->Range(8, 8 << 20);
 BENCHMARK(BM_HashSet_BuildFromVector)->Range(8, 8 << 20);
 
