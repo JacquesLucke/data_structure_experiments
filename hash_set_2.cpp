ca3ce0f098a837655de92c8b0f96ad3b190e3029
Jacques Lucke <mail@jlucke.com>
2019-07-29 19:24:08 +0200

    create slot iterator

--- hash_set_2.cpp
+++ hash_set_2.cpp
@@ -6,36 +6,43 @@
 typedef unsigned int uint;
 
 #define PERTURB_SHIFT 5
+#define OFFSET_MASK 3
 
-void handle(uint slot) {}
+// clang-format off
 
-void iter_slots(uint32_t hash, uint32_t group_mask,
-                uint32_t offset_mask) {
-    uint32_t perturb = hash;
-    do {
-        uint group = hash & group_mask;
-        uint offset_in_group = hash & offset_mask;
-        uint initial_offset = offset_in_group;
-        do {
-            uint slot = group | offset_in_group;
-            std::cout << "Group: " << group
-                      << "\tSlot: " << slot << '\n';
-            if (slot == 0) return;
-            offset_in_group =
-                (offset_in_group + 1) & offset_mask;
-        } while (offset_in_group != initial_offset);
-        perturb >>= PERTURB_SHIFT;
-        hash = hash * 5 + 1 + perturb;
+#define ITER_SLOTS_BEGIN(HASH, GROUP_MASK, R_SLOT)         \
+    uint32_t hash_ = HASH;                                 \
+    uint32_t perturb = HASH;                               \
+    do {                                                   \
+        uint group = hash_ & group_mask;                   \
+        uint offset_in_group = hash_ & OFFSET_MASK;        \
+        uint initial_offset = offset_in_group;             \
+        do {                                               \
+            uint32_t R_SLOT = group | offset_in_group;
+
+#define ITER_SLOTS_END                                             \
+            offset_in_group = (offset_in_group + 1) & OFFSET_MASK; \
+        } while (offset_in_group != initial_offset);               \
+        perturb >>= PERTURB_SHIFT;                                 \
+        hash_ = hash_ * 5 + 1 + perturb;                           \
     } while (true);
+
+// clang-format on
+
+void iter_slots(uint32_t hash, uint32_t exponent) {
+    uint32_t group_mask =
+        ((1 << exponent) - 1) & ~OFFSET_MASK;
+
+    ITER_SLOTS_BEGIN(hash, group_mask, slot) {
+        std::cout << "Slot: " << slot << '\n';
+        if (slot == 0) return;
+    }
+    ITER_SLOTS_END
 }
 
 int main(int argc, char const *argv[]) {
-    int size = 1 << 10;
-    uint32_t offset_mask = 3;
-    uint32_t group_mask = (size - 1) & ~offset_mask;
-
     srand(time(NULL));
     uint32_t hash = rand();
-    iter_slots(hash, group_mask, offset_mask);
+    iter_slots(hash, 4);
     return 0;
 }
